c02ce087bdc6c23d8c219075b73b61e0d5a5e382
ashiqur-r-biplop <biplops22@gmail.com>
2024-05-04 04:09:51 +0600

    back face flip are ready

--- script.js
+++ script.js
@@ -73,3 +73,53 @@ document.addEventListener("DOMContentLoaded", function () {
     animateText();
   }, 300);
 });
+// addressing Your Concerns js start
+document.getElementById("card-1").addEventListener("mouseover", () => {
+  const card = document.getElementById("card-1");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("card-1").addEventListener("mouseleave", () => {
+  const card = document.getElementById("card-1");
+  card.style.transform = "rotateY(0deg)";
+});
+document.getElementById("card-2").addEventListener("mouseover", () => {
+  const card = document.getElementById("card-2");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("card-2").addEventListener("mouseleave", () => {
+  const card = document.getElementById("card-2");
+  card.style.transform = "rotateY(0deg)";
+});
+document.getElementById("card-3").addEventListener("mouseover", () => {
+  const card = document.getElementById("card-3");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("card-3").addEventListener("mouseleave", () => {
+  const card = document.getElementById("card-3");
+  card.style.transform = "rotateY(0deg)";
+});
+document.getElementById("card-4").addEventListener("mouseover", () => {
+  const card = document.getElementById("card-4");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("card-4").addEventListener("mouseleave", () => {
+  const card = document.getElementById("card-4");
+  card.style.transform = "rotateY(0deg)";
+});
+document.getElementById("card-5").addEventListener("mouseover", () => {
+  const card = document.getElementById("card-5");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("card-5").addEventListener("mouseleave", () => {
+  const card = document.getElementById("card-5");
+  card.style.transform = "rotateY(0deg)";
+});
+document.getElementById("card-6").addEventListener("mouseover", () => {
+  const card = document.getElementById("card-6");
+  card.style.transform = "rotateY(180deg)";
+});
+document.getElementById("card-6").addEventListener("mouseleave", () => {
+  const card = document.getElementById("card-6");
+  card.style.transform = "rotateY(0deg)";
+});
+// addressing Your Concerns js end
