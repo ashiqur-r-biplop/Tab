4dbe70cb3f4afe4b4f99a8249866b6d5c293e0ab
ashiqur-r-biplop <biplops22@gmail.com>
2024-05-06 17:21:53 +0600

    added tab

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 // addressing Your Concerns js start
 // JavaScript fallback for hover effect
-document.querySelectorAll(".card").forEach(card => {
+document.querySelectorAll(".card").forEach((card) => {
   card.addEventListener("mouseenter", () => {
     card.querySelector(".card-content").style.transform = "rotateY(180deg)";
   });
@@ -86,3 +86,113 @@ document.querySelectorAll(".card").forEach(card => {
 });
 
 // addressing Your Concerns js end
+// tab js code
+function handleTab(boxId, TabIndex) {
+  const Tab = document.getElementById(TabIndex);
+  const TabBox = document.getElementById(boxId);
+  const activeTab = Tab?.id;
+  console.log(Tab);
+  if (activeTab == "tab-1" || TabIndex == "tab-1") {
+    const tabIndexTwo = document.getElementById("tab-box-2");
+    const tabIndexThree = document.getElementById("tab-box-3");
+    const tabIndexFour = document.getElementById("tab-box-4");
+    const tabIndexFive = document.getElementById("tab-box-5");
+    tabIndexTwo.classList.remove("tab-box-active");
+    tabIndexThree.classList.remove("tab-box-active");
+    tabIndexFour.classList.remove("tab-box-active");
+    tabIndexFive.classList.remove("tab-box-active");
+    TabBox.classList.add("tab-box-active");
+    // add tab active class
+    const tabTwo = document.getElementById("tab-2");
+    const tabThree = document.getElementById("tab-3");
+    const tabFour = document.getElementById("tab-4");
+    const tabFive = document.getElementById("tab-5");
+    tabTwo.classList.remove("activeTab");
+    tabThree.classList.remove("activeTab");
+    tabFour.classList.remove("activeTab");
+    tabFive.classList.remove("activeTab");
+    Tab.classList.add("activeTab");
+  } else if (activeTab == "tab-2") {
+    const tabIndexOne = document.getElementById("tab-box-1");
+    const tabIndexThree = document.getElementById("tab-box-3");
+    const tabIndexFour = document.getElementById("tab-box-4");
+    const tabIndexFive = document.getElementById("tab-box-5");
+    tabIndexOne.classList.remove("tab-box-active");
+    tabIndexThree.classList.remove("tab-box-active");
+    tabIndexFour.classList.remove("tab-box-active");
+    tabIndexFive.classList.remove("tab-box-active");
+    TabBox.classList.add("tab-box-active");
+    // add tab active class
+    const tabOne = document.getElementById("tab-1");
+    const tabThree = document.getElementById("tab-3");
+    const tabFour = document.getElementById("tab-4");
+    const tabFive = document.getElementById("tab-5");
+    tabOne.classList.remove("activeTab");
+    tabThree.classList.remove("activeTab");
+    tabFour.classList.remove("activeTab");
+    tabFive.classList.remove("activeTab");
+    Tab.classList.add("activeTab");
+  } else if (activeTab == "tab-3") {
+    const tabIndexOne = document.getElementById("tab-box-1");
+    const tabIndexTwo = document.getElementById("tab-box-2");
+    const tabIndexFour = document.getElementById("tab-box-4");
+    const tabIndexFive = document.getElementById("tab-box-5");
+    tabIndexOne.classList.remove("tab-box-active");
+    tabIndexTwo.classList.remove("tab-box-active");
+    tabIndexFour.classList.remove("tab-box-active");
+    tabIndexFive.classList.remove("tab-box-active");
+    TabBox.classList.add("tab-box-active");
+    // add tab active class
+    const tabOne = document.getElementById("tab-1");
+    const tabTwo = document.getElementById("tab-2");
+    const tabFour = document.getElementById("tab-4");
+    const tabFive = document.getElementById("tab-5");
+    tabOne.classList.remove("activeTab");
+    tabTwo.classList.remove("activeTab");
+    tabFour.classList.remove("activeTab");
+    tabFive.classList.remove("activeTab");
+    Tab.classList.add("activeTab");
+  } else if (activeTab == "tab-4") {
+    const tabIndexOne = document.getElementById("tab-box-1");
+    const tabIndexTwo = document.getElementById("tab-box-2");
+    const tabIndexThree = document.getElementById("tab-box-3");
+    const tabIndexFive = document.getElementById("tab-box-5");
+    tabIndexOne.classList.remove("tab-box-active");
+    tabIndexTwo.classList.remove("tab-box-active");
+    tabIndexThree.classList.remove("tab-box-active");
+    tabIndexFive.classList.remove("tab-box-active");
+    TabBox.classList.add("tab-box-active");
+    // add tab active class
+    const tabOne = document.getElementById("tab-1");
+    const tabTwo = document.getElementById("tab-2");
+    const tabThree = document.getElementById("tab-3");
+    const tabFive = document.getElementById("tab-5");
+    tabOne.classList.remove("activeTab");
+    tabTwo.classList.remove("activeTab");
+    tabThree.classList.remove("activeTab");
+    tabFive.classList.remove("activeTab");
+    Tab.classList.add("activeTab");
+  } else if (activeTab == "tab-5") {
+    const tabIndexOne = document.getElementById("tab-box-1");
+    const tabIndexTwo = document.getElementById("tab-box-2");
+    const tabIndexThree = document.getElementById("tab-box-3");
+    const tabIndexFour = document.getElementById("tab-box-4");
+    tabIndexOne.classList.remove("tab-box-active");
+    tabIndexTwo.classList.remove("tab-box-active");
+    tabIndexThree.classList.remove("tab-box-active");
+    tabIndexFour.classList.remove("tab-box-active");
+    TabBox.classList.add("tab-box-active");
+    // add tab active class
+    const tabOne = document.getElementById("tab-1");
+    const tabTwo = document.getElementById("tab-2");
+    const tabThree = document.getElementById("tab-3");
+    const tabFour = document.getElementById("tab-4");
+
+    tabOne.classList.remove("activeTab");
+    tabTwo.classList.remove("activeTab");
+    tabThree.classList.remove("activeTab");
+    tabFour.classList.remove("activeTab");
+    Tab.classList.add("activeTab");
+  }
+}
+handleTab("tab-box-1", "tab-1");
